50388e356e74c6278049511f96327c4810f3d76d
Devadoss <devadossp@gmail.com>
2017-03-23 17:50:35 +0530

    changed classa

--- ClassA.java
+++ ClassA.java
@@ -3,7 +3,10 @@ package com.test;
 Public class ClassA {
 
 	public void method1(){
-		System.out.println("method1");
+		System.out.println("inside method1");
 	}
 
+	public void method2(){
+		System.out.println("method2");
+	}
 }
